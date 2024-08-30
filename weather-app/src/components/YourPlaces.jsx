60d2d70cb79c6a8196b13b6287ef856d79b53d9f
RiccardoMamoli <riccardomamoli2@gmail.com>
2024-08-30 17:04:16 +0200

    Update YourPlaces.jsx

--- weather-app/src/components/YourPlaces.jsx
+++ weather-app/src/components/YourPlaces.jsx
@@ -4,11 +4,9 @@ import { Row, Col, Container } from 'react-bootstrap';
 import SingleCity from './SingleCity';
 
 function YourPlaces() {
-    // Stato inizializzato come array
     const [cities, setCities] = useState([]);
 
     const handleSearch = (query) => {
-        // Aggiungi la città all'array solo se non è già presente e se non è vuota
         if (query && !cities.includes(query)) {
             setCities([...cities, query]);
         }
@@ -16,9 +14,11 @@ function YourPlaces() {
 
     return (
         <>
-            <CustomSearchBar onSearch={handleSearch} />
+            <div>
+                <CustomSearchBar onSearch={handleSearch} />
+            </div>
             <Container fluid className="bg-dark text-light mt-2 d-flex h-100 align-items-center">
-                <Row>
+                <Row className='w-100'>
                     {cities.map((city, index) => (
                         <Col xs={12} lg={3} className="d-flex align-items-center" key={index}>
                             <SingleCity city={city} />
